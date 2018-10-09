0a5c77b5ec68a95b1a48e5a0456f8f96150d4b1b
Matheus Hunsche <matheusarendthunsche@gmail.com>
2018-10-09 19:11:42 -0300

    Update Ragna.State.pas

--- src/Ragna.State.pas
+++ src/Ragna.State.pas
@@ -3,22 +3,25 @@ unit Ragna.State;
 interface
 
 uses
-  System.Generics.Collections, FireDac.Comp.Client;
+  System.Generics.Collections, FireDac.Comp.Client, System.Rtti;
 
 type
 
-  TPairOfQueryAndSql = TPair<TFDQuery, string>;
+  TListQueryAndSql = TDictionary<TFDQuery, string>;
 
   TRagnaState = class
   private
     FSecret: string;
-    FStates: TThreadList<TPairOfQueryAndSql>;
+    FStates: TListQueryAndSql;
+    FVmi : TVirtualMethodInterceptor;
     class var FInstance: TRagnaState;
+    procedure OnBeforVMI(Instance: TObject;
+    Method: TRttiMethod; const Args: TArray<TValue>; out DoInvoke: Boolean;
+    out Result: TValue);
   public
-    property States: TThreadList<TPairOfQueryAndSql> read FStates write FStates;
+    property States: TListQueryAndSql read FStates write FStates;
     procedure SetState(AQuery: TFDQuery; ASQL: string);
-    function GetState(AQuery: TFDQuery): string;
-    function RemoveState(AQuery: TFDQuery): string; deprecated;
+    function GetState(AQuery: TFDQuery; out ASQL: string): Boolean;
     class function GetInstance: TRagnaState;
     class procedure Release;
     constructor Create;
@@ -27,16 +30,20 @@ type
 
 implementation
 
+
 { TRagnaState }
 
 constructor TRagnaState.Create;
 begin
-  FStates := TThreadList<TPairOfQueryAndSql>.Create;
+  FVmi := TVirtualMethodInterceptor.Create(TFDQuery);
+  FVmi.OnBefore := OnBeforVMI;
+  FStates := TListQueryAndSql.Create;
 end;
 
 destructor TRagnaState.Destroy;
 begin
   FStates.Free;
+  FVmi.Free;
 end;
 
 class function TRagnaState.GetInstance: TRagnaState;
@@ -46,23 +53,26 @@ begin
   Result := FInstance;
 end;
 
-function TRagnaState.GetState(AQuery: TFDQuery): string;
-var
-  LState: TPairOfQueryAndSql;
-  LStates: TList<TPairOfQueryAndSql>;
+function TRagnaState.GetState(AQuery: TFDQuery; out ASQL: string): Boolean;
 begin
-  LStates := FStates.LockList;
+  TMonitor.Enter(FStates);
   try
-    for LState in LStates do
-    begin
-      if LState.Key = AQuery then
-      begin
-        Result := LState.Value;
-        Break;
-      end;
-    end;
+    Result := FStates.TryGetValue(AQuery, ASQL);
   finally
-    FStates.UnlockList;
+    TMonitor.Exit(FStates);
+  end;
+end;
+
+procedure TRagnaState.OnBeforVMI(Instance: TObject; Method: TRttiMethod;
+  const Args: TArray<TValue>; out DoInvoke: Boolean; out Result: TValue);
+begin
+  if Method.Name <> 'BeforeDestruction' then
+    Exit;
+  TMonitor.Enter(FStates);
+  try
+    FStates.Remove(Instance as TFDQuery);
+  finally
+    TMonitor.Exit(FStates);
   end;
 end;
 
@@ -71,34 +81,16 @@ begin
   FInstance.Free;
 end;
 
-function TRagnaState.RemoveState(AQuery: TFDQuery): string;
-var
-  LState: TPairOfQueryAndSql;
-  LStates: TList<TPairOfQueryAndSql>;
-begin
-  LStates := FStates.LockList;
-  try
-    for LState in LStates do
-    begin
-      if LState.Key = AQuery then
-      begin
-        Result := LState.Value;
-        Break;
-      end;
-    end;
-  finally
-    FStates.UnlockList;
-  end;
-
-  LStates.Remove(LState);
-end;
-
 procedure TRagnaState.SetState(AQuery: TFDQuery; ASQL: string);
-var
-  LState: TPairOfQueryAndSql;
 begin
-  LState := TPairOfQueryAndSql.Create(AQuery, ASQL);
-  FStates.Add(LState);
+  TMonitor.Enter(FStates);
+  try
+    if FVmi.ProxyClass <> AQuery.ClassType then
+      FVmi.Proxify(AQuery);
+    FStates.AddOrSetValue(AQuery, ASQL);
+  finally
+    TMonitor.Exit(FStates);
+  end;
 end;
 
 initialization
